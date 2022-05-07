8b6c366802cdc33284d6133af721d7fc3b8e337e
dubisdev <dubisdev@gmail.com>
2022-05-08 01:36:53 +0200

    ci: fix build

---
+++ esbuild.js
@@ -0,0 +1,22 @@
+import esbuild from "esbuild";
+
+esbuild
+  .build({
+    platform: "node",
+    logLevel: "info",
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    minify: true,
+    format: "esm",
+    target: "node16",
+    outfile: "dist/index.js",
+    external: [
+      "twitter-autohook",
+      "axios",
+      "dotenv",
+      "jsonwebtoken",
+      "todoist-rest-client",
+      "twitter-api-v2",
+    ],
+  })
+  .catch(() => process.exit(1));
