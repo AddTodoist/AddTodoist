c231e19cec77d209b30441684b9282d6e1437bbd
dubisdev <dubisdev@gmail.com>
2022-05-08 17:28:45 +0200

    chroe: fail indpendently

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,24 @@ process.on("SIGTERM", () => process.exit(0));
 dotenv.config();
 console.clear();
 
-const OAuthServer = await setupOAuthServer();
-const TWAutohookServer = await setupAutohookServer();
+try {
+  const OAuthServer = await setupOAuthServer();
+  OAuthServer.on("error", (err) => {
+    OAuthServer.close();
+    console.error("OAuthServer error:", err);
+    console.error("OAuthServer is down");
+  });
+} catch (err) {
+  console.error("OAuthServer setup failed", err);
+}
+
+try {
+  const TWAutohookServer = await setupAutohookServer();
+  TWAutohookServer.on("error", (err) => {
+    console.error("TWAutohookServer error:", err);
+    TWAutohookServer.removeWebhooks();
+    console.error("TWAutohookServer is down");
+  });
+} catch (err) {
+  console.error("TWAutohookServer setup failed", err);
+}
