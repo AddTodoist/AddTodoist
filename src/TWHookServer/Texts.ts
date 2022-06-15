1535b9ef988d0e5147084abc50c31fdd26912e2b
dubisdev <dubisdev@gmail.com>
2022-06-15 18:30:00 +0200

    chore: update texts

--- src/TWHookServer/Texts.ts
+++ src/TWHookServer/Texts.ts
@@ -7,11 +7,11 @@ enum TWHookServerTexts {
 
 export const generateInitText = (userId: string) => `
 🔴 Hi there! 🔴\n\
-I'm SaveToTodoist bot and I'm here to help you save tweets and threads to your Todoist account.\n\
+I'm AddToDoist bot and I'm here to help you save tweets and threads to your Todoist account.\n\
 \nFirst of all, you must cofigure me for making me able to access your account.\n\n\
-👉Follow this steps👈\n\
-1. Go to https://todoist.com/oauth/authorize?client_id=${process.env.TODOIST_CLIENT_ID}&scope=data:read_write&state=${userId} and get your todoist account token\n\
-2. Grant app permissions:\n\
+Follow this steps:\n\
+1. Go to https://todoist.com/oauth/authorize?client_id=${process.env.TODOIST_CLIENT_ID}&scope=data:read_write&state=${userId} and get your Todoist account token.\n\
+2. Grant app permissions.\n\
 3. When you are done I will come back to set up your projects cofiguration.`;
 
 export default TWHookServerTexts;
