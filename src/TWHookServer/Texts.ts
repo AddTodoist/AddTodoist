8cf995fb1877f2e3025afa16867b1e81d6c43a89
David Jiménez <dubisdev@gmail.com>
2022-08-18 22:01:41 +0200

    fix: Fit user permissions to real app needs

--- src/TWHookServer/Texts.ts
+++ src/TWHookServer/Texts.ts
@@ -14,7 +14,7 @@ export const generateInitText = (userId: string) => `
 I'm AddTodoist bot and I'm here to help you save tweets and threads to your Todoist account.\n\
 \nFirst of all, you must cofigure me for making me able to access your account.\n\n\
 Follow this steps:\n\
-1. Go to https://todoist.com/oauth/authorize?client_id=${process.env.TODOIST_CLIENT_ID}&scope=data:read_write&state=${userId}\n\
+1. Go to https://todoist.com/oauth/authorize?client_id=${process.env.TODOIST_CLIENT_ID}&scope=task:add,data:read&state=${userId}\n\
 2. Grant app permissions.\n\
 3. When you are done I will come back to set up your projects cofiguration.`;
 
