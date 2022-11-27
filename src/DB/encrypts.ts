b5c6496c34041a642e72e282be6571d342ed2322
dubisdev <dubisdev@gmail.com>
2022-11-27 18:28:22 +0100

    chore: remove moved code

--- src/DB/encrypts.ts
+++
@@ -1,9 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-const secret = process.env.DB_SECRET || null;
-if (!secret) throw new Error('DB_SECRET is not set');
-
-export const hashId = (id: string) => CryptoJS.SHA256(id).toString();
-
-export const encryptString = (data: string) => CryptoJS.AES.encrypt(data, secret).toString();
-export const decryptString = (data: string) => CryptoJS.AES.decrypt(data, secret).toString(CryptoJS.enc.Utf8);
--- src/bugsnag.ts
+++
@@ -1,5 +0,0 @@
-import Bugsnag from '@bugsnag/js';
-
-Bugsnag.start(process.env.BUGSNAG_API_KEY || '');
-
-export default Bugsnag;
